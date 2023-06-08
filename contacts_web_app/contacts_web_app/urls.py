96c9217b11149d16d94c0e152b2477d299ac609c
Denys Zaitsev <denzai83@gmail.com>
2023-06-09 00:16:02 +0300

    Add notes app

--- contacts_web_app/contacts_web_app/urls.py
+++ contacts_web_app/contacts_web_app/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('users.urls', namespace='default')),
     path('users/', include('users.urls')),
+    path('notes/', include('notes.urls')),
 ]
