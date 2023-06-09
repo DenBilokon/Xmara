a94a781db17efb9332ceccc7d77179403847b5d9
Denis Bilokon <greenjuiced@gmail.com>
2023-06-09 17:27:25 +0300

    add weather, add icons

--- contacts_web_app/file_manager/views.py
+++ contacts_web_app/file_manager/views.py
@@ -1,6 +1,12 @@
+from datetime import date
+
 from django.shortcuts import render
+from users.views import currency_parse
 
 
 def main_mf(request):
-    return render(request, 'file_manager/index.html')
+    currency_info = currency_parse()
+    return render(request, 'file_manager/index.html', context={'currency_info': currency_info,
+                                                               'date': date.today().strftime('%d.%m.%Y')})
+
 
