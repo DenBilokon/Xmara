b26639d59c0d41586ea3d85f946f28a4f801ea0e
Sanyavas <vasilinaoleksandr@gmail.com>
2023-06-10 22:46:48 +0300

    test merge

---
+++ contacts_web_app/file_manager/forms.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm, TextInput, ClearableFileInput
+
+from .models import Library
+
+
+class LibraryForm(ModelForm):
+    class Meta:
+        model = Library
+        fields = ['title', 'description', 'image']
+        widgets = {
+            'title': TextInput(attrs={'class': 'form-control'}),
+            'description': TextInput(attrs={'class': 'form-control'}),
+            'image': ClearableFileInput(attrs={'class': 'form-control'}),
+        }
--- contacts_web_app/file_manager/views.py
+++ contacts_web_app/file_manager/views.py
@@ -1,10 +1,14 @@
 from datetime import date
 
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from users.views import currency_parse, weather_parse
 from .models import Library
+from .forms import LibraryForm
 
 
+@login_required
 def main_mf(request):
     currency_info = currency_parse()
     weather_info = weather_parse()
@@ -14,10 +18,35 @@ def main_mf(request):
 
 
 def images(request):
-    cloud_images = Library.objects.all()
+    user_id = request.user.id
+    cloud_images = Library.objects.filter(user_id=user_id)
+    if request.method == 'POST':
+        form = LibraryForm(request.POST)
+        if form.is_valid():
+            library = form.save(commit=False)
+            library.user = request.user
+            library.save()
+            return redirect('library_detail', library_id=library.id)
+
+    form = LibraryForm()
     currency_info = currency_parse()
     weather_info = weather_parse()
     return render(request, 'file_manager/images.html', context={'currency_info': currency_info,
-                                                               'date': date.today().strftime('%d.%m.%Y'),
-                                                               'weather_info': weather_info,
-                                                               'cloud_images': cloud_images})
+                                                                'date': date.today().strftime('%d.%m.%Y'),
+                                                                'weather_info': weather_info,
+                                                                'cloud_images': cloud_images,
+                                                                'form': form})
+
+
+def create_library(request):
+    if request.method == 'POST':
+        form = LibraryForm(request.POST)
+        if form.is_valid():
+            library = form.save(commit=False)
+            library.user = request.user
+            library.save()
+            return redirect('library_detail', library_id=library.id)
+    else:
+        form = LibraryForm()
+
+    return render(request, 'create_library.html', {'form': form})
