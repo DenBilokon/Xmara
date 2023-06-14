393ed258905f1d102d15e74cb485340702948d5a
Denis Bilokon <greenjuiced@gmail.com>
2023-06-14 14:55:31 +0300

    add chat-gpt

--- contacts_web_app/news/urls.py
+++ contacts_web_app/news/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import home, news_war, news_war_show_one, news_prosport, news_prosport_show_one, war_statistic
+from .views import home, news_war, news_war_show_one, news_prosport, news_prosport_show_one, war_statistic, when_bored
 
 
 app_name = "news"
@@ -12,5 +12,6 @@ urlpatterns = [
     path('news_prosport/', news_prosport, name='news_prosport'),
     path('news_prosport/<int:_id>', news_prosport_show_one, name='news_prosport_show_one'),
     path('war_statistic/', war_statistic, name='war_statistic'),
+    path('bored/', when_bored, name='when_bored')
 
 ]
