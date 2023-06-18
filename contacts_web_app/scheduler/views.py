308460fd263d10f0bb50499c8747bd2566be3ba1
Denis Bilokon <greenjuiced@gmail.com>
2023-06-18 18:45:28 +0300

    fix avatar, add documentation to users/views

--- contacts_web_app/scheduler/views.py
+++ contacts_web_app/scheduler/views.py
@@ -8,7 +8,7 @@ from news.views import add_currency_to_file, add_crypto_currency_to_file
 
 def start():
     """
-    The start function is called by the scheduler.py file, which is run as a cron job every minute.
+    The start function is called by the scheduler.py file, which is run as a cron job every day.
     
     :return: None
     :doc-author: Trelent
